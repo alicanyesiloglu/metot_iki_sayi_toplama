87a963dbea71423a7fe595e9ad921739f7ef4d32
alican <doc.alican@hotmail.com>
2019-02-16 21:42:42 +0300

    metot ile klavyeden girilen iki sayıyı toplama

---
+++ ders 44 ödev metot 2 sayı toplama/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders_44_ödev_metot_2_sayı_toplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         int toplam(int t1,int t2)
+        {
+            int f = t1 + t2;
+            return f;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int q = Convert.ToInt16(textBox1.Text);
+            int qq = Convert.ToInt16(textBox2.Text);
+
+            label1.Text = toplam(q,qq).ToString();
+        }
+    }
+}
